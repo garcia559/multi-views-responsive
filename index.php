6f2790de6cf0fc88a79c32ee06cafea8bff0decd
Jorge Garcia <jorge@eurekacrew.com>
2018-08-24 16:50:02 -0500

    Primera subida de los archivos hasta ahora Maquetados

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800" rel="stylesheet">
+	<link rel="stylesheet" href="css/custom.css">
+	<title>Multipantllas</title>
+</head>
+<body>
+	<div class="contentAll">
+		<h1>VISTAS RESPONSIVE</h1>
+
+		<div class="contentViews">
+			<div class="column">
+				<div class="view cellH">
+					<h3>Celular Horizontal</h3>
+					<p>600 x 280 px</p>
+
+					<br>
+
+					<iframe src="http://localhost:8000/CARAL-web-villaconvivium/" frameborder="0"></iframe>
+				</div>
+
+				<br>
+
+				<div class="view cellVP">
+					<h3>Celular Vertical Pequeño</h3>
+					<p>360 x 560 px</p>
+
+					<br>
+
+					<iframe src="http://localhost:8000/CARAL-web-villaconvivium/" frameborder="0"></iframe>
+				</div>
+			</div>
+
+			<div class="column">
+				<div class="view cellVG">
+					<h3>Celular Vertical Grande</h3>
+					<p>480 x 560 px</p>
+
+					<br>
+
+					<iframe src="http://localhost:8000/CARAL-web-villaconvivium/" frameborder="0"></iframe>
+				</div>
+			</div>
+
+			<div class="column">
+				<div class="view miniIpadV">
+					<h3>Mini Ipad Vertical</h3>
+					<p>768 x 909 px</p>
+
+					<br>
+
+					<iframe src="http://localhost:8000/CARAL-web-villaconvivium/" frameborder="0"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ iphones.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800" rel="stylesheet">
+	<link rel="stylesheet" href="css/custom.css">
+	<title>Multipantllas</title>
+</head>
+<body>
+	<div class="contentAll">
+		<h1>VISTAS RESPONSIVE</h1>
+
+		<div class="contentViews">
+
+
+			<div class="column">
+				<div class="view cellVP">
+					<h3>Celular Vertical Pequeño</h3>
+					<p>360 x 560 px</p>
+
+					<br>
+
+					<iframe src="http://localhost:8000/CARAL-web-villaconvivium/" frameborder="0"></iframe>
+				</div>
+			</div>
+
+			<div class="column">
+				<div class="view">
+					<h3>iPhone X</h3>
+					<p>Vertical</p>
+					<p>375 x 635</p>
+
+					<br>
+
+					<iframe src="http://localhost:8000/CARAL-web-villaconvivium/" frameborder="0" style="width:375px;height:635px;"></iframe>
+				</div>
+			</div>
+
+			<div class="column">
+				<div class="view">
+					<h3>iPhone 8 Plus && iPhone 7 Plus</h3>
+					<p>Vertical</p>
+					<p>414 x 622</p>
+
+					<br>
+
+					<iframe src="http://localhost:8000/CARAL-web-villaconvivium/" frameborder="0" style="width:414px;height:622px;"></iframe>
+				</div>
+			</div>
+
+			<!--<div class="column">
+				<div class="view">
+					<h3>iPhone X</h3>
+					<p>Horizontal</p>
+					<p>724 x 375</p>
+
+					<br>
+
+					<iframe src="http://localhost:8000/CARAL-web-villaconvivium/" frameborder="0" style="width:724px;height:375px;"></iframe>
+				</div>
+
+				<br>
+
+				<div class="view">
+					<h3>iPhone 8 Plus && iPhone 7 Plus</h3>
+					<p>Horizontal</p>
+					<p>736 x 414</p>
+
+					<br>
+
+					<iframe src="http://localhost:8000/CARAL-web-villaconvivium/" frameborder="0" style="width:724px;height:375px;"></iframe>
+				</div>
+			</div>-->
+		</div>
+	</div>
+</body>
+</html>
